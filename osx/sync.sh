37e7d0b70d8900e19217d3e4b7689f5aa605bed6
sorokan6 <andrei.sorokin_ext@novartis.com>
2022-12-09 15:51:50 +0100

    crontab

--- osx/sync.sh
+++ osx/sync.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-rsync --archive --update --stats --delete /Users/sorokan6/Documents/ "/Users/sorokan6/Library/CloudStorage/OneDrive-Personal/Documents"
+echo ""  >> /Users/sorokan6/sync.log
 date >> /Users/sorokan6/sync.log
+
+rsync \
+        --archive \
+        --update \
+        --stats \
+        --delete \
+        /Users/sorokan6/Documents/ \
+        "/Users/sorokan6/Library/CloudStorage/OneDrive-Personal/Documents" \
+        >> /Users/sorokan6/sync.log
+
+rsync \
+        --archive \
+        --update \
+        --stats \
+        --delete \
+        "/Users/sorokan6/сербский/" \
+        "/Users/sorokan6/Library/CloudStorage/OneDrive-Personal/сербский" \
+        >> /Users/sorokan6/sync.log
