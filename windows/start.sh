d5178422ddba5f0930f5c148bcc9d363c9405124
andgineer <andrey@sorokin.engineer>
2023-01-13 14:07:09 +0100

    removed outdated configs

--- windows/start.sh
+++
@@ -1,4 +0,0 @@
-docker build . --tag=ubuntu
-docker run -it --name=ubuntu --privileged --network=host -v "C:\Users\Andrei_Sorokin2\projects":/projects ubuntu
-# service docker start
-docker start -i ubuntu
