37060420cc29da318b54b31f11f18906f2795246
andgineer <Andrei_Sorokin2@epam.com>
2025-10-13 06:26:08 +0200

    other to roller ski

--- osx/garmin_daily.sh
+++ osx/garmin_daily.sh
@@ -11,7 +11,7 @@ source "${home}/.garminsecrets"
 
 echo $(date) "garmin-daily" >> "${sync_log}"
 "/Users/${IAM}/projects/garmin-daily/.venv/bin/garmin-daily" \
-  -g mon -g wed -g fri -g sat -l "roller=Телеп" -l "trail=Телеп" -r "trail=Roller skiing" \
+  -g mon -g wed -g fri -g sat -l "roller=Телеп" -l "other=Телеп" -r "other=Roller skiing" \
   >> "${sync_log}"
 
 if [ $? -ne 0 ]; then
